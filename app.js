83588e648be475546dd8a9df060fe5b1479647f1
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-08 22:11:18 +0430

    new changes on js file

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const parent=document.getElementById("box");
+
+const hour= document.createElement("span");
+const minutes= document.createElement("span");
+const seconds = document.createElement("span");
+function displayTime(){
+   const now=new Date();
+   const sec=now.getSeconds();
+   const min= now.getMinutes();
+   const hours=now.getHours();
+   if(sec<10){
+    sec= "0"+sec;
+   }
+   if(min<10){
+    min= "0"+min;
+   }
+   if(hours<10){
+    hours="0"+hours;
+   }
+}
