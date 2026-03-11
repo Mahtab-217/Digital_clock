bd64fa6acb67bc1e7c61e641b40530b3ac74fb18
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-11 20:46:35 +0430

    getting data from database

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const container=document.getElementById("container");
 async function getData(){
-    const Info = await fetch("https://jsonplacholder.typicode.com/users");
+    const Info = await fetch("https://jsonplaceholder.typicode.com/users");
     const response=await Info.json();
     for(let user of response){
        const name= document.createElement("h1");
        const email= document.createElement("p");
        const phone= document.createElement("a");
+       phone.setAttribute("href",`tel: ${user.phone}`);
+       name.textContent= user.name;
+       email.textContent= user.email;
+       phone.textContent= user.phone;
+
+       container.append(name,email,phone);
     }
 }
 getData();
