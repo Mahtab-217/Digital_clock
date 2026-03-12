80b57870668ca9e3c4e923ea7aa165aa5aa61bfa
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-12 20:16:52 +0430

    recursive functions

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const container=document.getElementById("container");
-async function getData(){
-    const Info = await fetch("https://jsonplaceholder.typicode.com/users");
-    const response=await Info.json();
-    for(let user of response){
-       const name= document.createElement("h1");
-       const email= document.createElement("p");
-       const phone= document.createElement("a");
-       phone.setAttribute("href",`tel: ${user.phone}`);
-       name.textContent= user.name;
-       email.textContent= user.email;
-       phone.textContent= user.phone;
-     const div=document.createElement("div")
-       div.append(name,email,phone);
-       div.style.cssText= "width:80%; margin:8px auto; box-shadow:2px 2px 10px rgba(0,0,0,0.8); padding:12px;";
-       container.appendChild(div)
-    }
+// const container=document.getElementById("container");
+// async function getData(){
+//     const Info = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const response=await Info.json();
+//     for(let user of response){
+//        const name= document.createElement("h1");
+//        const email= document.createElement("p");
+//        const phone= document.createElement("a");
+//        phone.setAttribute("href",`tel: ${user.phone}`);
+//        name.textContent= user.name;
+//        email.textContent= user.email;
+//        phone.textContent= user.phone;
+//      const div=document.createElement("div");
+//        div.append(name,email,phone);
+//        div.style.cssText= "width:80%; margin:8px auto; box-shadow:2px 2px 10px rgba(0,0,0,0.8); padding:12px;";
+//        container.appendChild(div);
+//     }
+// }
+// getData();
+let num=0;
+function greeting(){
+  console.log("Show me");
+  if(num==5){
+    return;
+  }
+    greeting(); 
 }
-getData();
+greeting();
